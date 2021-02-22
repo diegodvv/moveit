b3a49117f0ebf02327d39c63deb61b84223e6bbf
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-22 20:31:11 -0300

    Style ExperienceBar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -2,7 +2,11 @@ export const ExperienceBar = () => {
   return (
     <header className="experience-bar">
       <span>0 xp</span>
-      <div />
+      <div>
+        <div style={{ width: '50%' }}>
+          <span className="current-experience" style={{left: '50%'}}>300 xp</span>
+        </div>
+      </div>
       <span>600 xp</span>
     </header>
   )
