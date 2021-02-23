0fd3eed4a513cff883d2a1dff298498013d5532e
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-23 17:27:20 -0300

    Add typescript and eslint to project

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,11 +3,11 @@ import styles from '../styles/components/Profile.module.css';
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/diegodvv.png" alt="Diego Vieira" />
+      <img src='https://github.com/diegodvv.png' alt='Diego Vieira' />
       <div>
         <strong>Diego Vieira</strong>
         <p>Level 1</p>
       </div>
     </div>
-  )
+  );
 }
