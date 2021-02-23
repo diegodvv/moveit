ea57738edb3598ebcaa6b65214c94d8e6f9c4ae2
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-23 17:15:33 -0300

    Style Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <img src="https://github.com/diegodvv.png" alt="Diego Vieira" />
       <div>
         <strong>Diego Vieira</strong>
