d069d94d22823a4e4a94af48cc7128b66ffc21d4
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-23 17:08:44 -0300

    Create Profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/diegodvv.png" alt="Diego Vieira" />
+      <div>
+        <strong>Diego Vieira</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
