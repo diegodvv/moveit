9abbfd59b5414dc9f3881760b1f9a3ce12df472a
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-24 21:51:43 -0300

    Create component ChallengesProvider

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,3 +1,26 @@
-import { createContext } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
 export const ChallengesContext = createContext({});
+
+type Props = {
+  children: ReactNode;
+};
+export function ChallengesProvider({ children }: Props) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {
+    console.log('new challenge');
+  }
+  return (
+    <ChallengesContext.Provider
+      value={{ level, levelUp, currentExperience, challengesCompleted, startNewChallenge }}>
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { useState } from 'react';
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [level, setLevel] = useState(1);
   return (
-    <ChallengesContext.Provider value={{ level: 1 }}>
+    <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesContext.Provider>
+    </ChallengesProvider>
   );
 }
 
