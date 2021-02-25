3804d162f9aa4ab916b247df93ee263be853e849
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-24 22:02:52 -0300

    Show dynamic data in ChallengeBox

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,9 +1,17 @@
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
+
+type Challenge = {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+};
 
 type ChallengeContextData = {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 };
@@ -16,17 +24,20 @@ export function ChallengesProvider({ children }: Props) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('new challenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+    setActiveChallenge(challenge);
   }
   return (
     <ChallengesContext.Provider
-      value={{ level, levelUp, currentExperience, challengesCompleted, startNewChallenge }}>
+      value={{ level, levelUp, currentExperience, challengesCompleted, activeChallenge, startNewChallenge }}>
       {children}
     </ChallengesContext.Provider>
   );
