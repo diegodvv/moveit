cce35deba40af4aeed8f4a4ca3d3778d33b549f6
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-24 21:46:55 -0300

    Create ChallengesContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { useState } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const [level, setLevel] = useState(1);
+  return (
+    <ChallengesContext.Provider value={{ level: 1 }}>
+      <Component {...pageProps} />
+    </ChallengesContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
