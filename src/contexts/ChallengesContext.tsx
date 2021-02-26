04f364562e540265de185c04a6927d3760fca4fb
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-25 22:34:39 -0300

    Play audio when starting new challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -45,6 +45,8 @@ export function ChallengesProvider({ children }: Props) {
     // @ts-expect-error
     setActiveChallenge(challenge);
 
+    new Audio('/notification.mp3').play();
+
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio 🎉', {
         body: `Valendo ${challenge.amount} xp!`,
