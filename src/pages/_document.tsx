bf0cf5e6883fea1ab8ccebc31900cfbc05a8e829
Diego Vieira <30930947+diegodvv@users.noreply.github.com>
2021-02-28 20:17:15 -0300

    Fix errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-          <meta
-            name='viewport'
-            content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
-          />
           <meta name='description' content='Exercise more while you code!' />
           <link rel='manifest' href='/manifest.json' />
           <link href='/favicon/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
